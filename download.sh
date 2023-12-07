fb48a3303e5481fe46880ef233e50ac8c51ebd46
Jia-Wei-Liao <sam23582211@gmail.com>
2023-12-07 14:07:09 +0800

    Move unzip to download.sh.

--- download.sh
+++ download.sh
@@ -4,6 +4,14 @@ if [ ! -d data ]; then
     gdown https://drive.google.com/uc?id=10J_nF3pfthiHac7b1uGVNJE0MYGcqpQS -O data.zip
 fi
 
-if [ ! -d checkpoint ]; then
+if [ ! -d data ]; then
+    unzip data.zip
+fi
+
+if [ ! -d best_checkpoint ]; then
     gdown https://drive.google.com/uc?id=1jpTbxkwClEOh5Au6TiCI3ZnfAQ9-FDgy -O best_checkpoint.zip
+fi
+
+if [ ! -d best_checkpoint ]; then
+    unzip best_checkpoint.zip
 fi
--- run.sh
+++ run.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-if [ ! -d data ]; then
-    unzip data.zip
-fi
-
-if [ ! -d best_checkpoint ]; then
-    unzip best_checkpoint.zip
-fi
-
-wait
-
 python infer.py --data_path "${1}" --output_path "${2}" --batch_size 10
